3e77190d9bfaa1f47e206b04cc779f0d07ca4a67
RobertKraaijeveld <0890289@hr.nl>
2015-10-14 15:14:49 +0200

    files

--- input_code_prototype/main.cpp
+++ input_code_prototype/main.cpp
@@ -1,9 +1,69 @@
-#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include "stdafx.h"
+#include <string>
+#include <cstdio>
+#include <ctime>
+#include <filesystem>
+#include <iostream>
+#include <dirent.h>
 
 using namespace std;
+using namespace std::tr2::sys;
+
+//DIRECTORY LISTENING
+
+//Nota: In C++ moet je als een methode een andere methode callt, de methode die gecallt word BOVEN de "caller" zetten
+static vector <string> getFileList()
+{
+	//define pointer + list 
+	DIR *dpdf;
+	struct dirent *epdf;
+	vector <string> dlist;
+
+	//check current dir
+	dpdf = opendir("./");
+
+	//if not empty
+	if (dpdf != NULL)
+	{
+	   //loop through dir
+	   while (epdf = readdir(dpdf))
+	   {
+		  //push every file into list
+		  dlist.push_back(epdf->d_name);
+	   }
+	}
+	closedir(dpdf);
+	return dlist;
+}
+
+static string checkEvery2Secs()
+{
+	//start clock, create vector(basically the same as a Java-style list) with files at 00:00
+	std::clock_t time;
+	vector <string> oldlist = getFileList();
+    time = std::clock();
+	
+	//every 2 secs check if folder got new files
+	if(time % 100 == 2)
+	{
+		//create new list with folder size at 02:00
+		vector <string> newlist = getFileList();
+		if(newlist.size() > oldlist.size())
+		{
+			//get last elem: cant select elem like this
+			string filePath = newlist[newlist.size()];
+			//TODO: send file of that elem through to cj
+			//reset the oldlist
+			oldlist = newlist;
+		}
+	}
+	return filePath;
+}
+
+//CSV PARSING
 
 class CSVRow {
 public:
@@ -39,7 +99,7 @@ istream & operator >>(std::istream & str, CSVRow& data){
 
 int main() {
 
-    ifstream file("C:/something.csv");
+    ifstream file(checkEvery2Secs);
 
     CSVRow row;
     while (file >> row){
