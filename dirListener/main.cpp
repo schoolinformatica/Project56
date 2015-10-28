c4cc60b5a42b5734a43b8c3579bf357ee00eebdf
Robert <0890289@hr.nl>
2015-10-28 13:14:18 +0100

    dirlistener

--- dirListener/main.cpp
+++ dirListener/main.cpp
@@ -2,12 +2,14 @@
 #include <thread>
 #include <dirent.h>
 #include <vector>
-#include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
 using namespace std;
 
-//this method returns a vector containg all the files in the given directory.
+//this method returns a vector containing all the files in the given directory.
 vector<const char*> getDirFileList(const char* directory)
 {
     DIR *dir;
@@ -20,7 +22,7 @@ vector<const char*> getDirFileList(const char* directory)
         while ((ent = readdir (dir)) != NULL)
         {
             FileList.push_back(ent->d_name);
-            printf(ent->d_name);
+            printf("%s\n", ent->d_name);
         }
         //When we scanned all the files, we close the directory and return the list
         closedir (dir);
@@ -38,22 +40,34 @@ int main()
 {
     while(true)
     {
-        //We create a vector that holds the list of files and dirs BEFORE the 2 second loop
-        vector<const char*> oldFileList = getDirFileList("/home/cooperatio/");
-        thread t1(getDirFileList, "/home/cooperatio/");
+        thread t1(getDirFileList, "/home/robert/TESTFOLDER/");
         t1.join();
+        //We create a vector that holds the list of files and dirs BEFORE the 2 second loop
+        vector<const char*> oldFileList = getDirFileList("/home/robert/TESTFOLDER/");
         sleep(2);
         //We redo this procedure AFTER the 2 seconds have passed
-        vector<const char*> newFileList = getDirFileList("/home/cooperatio/");
+        vector<const char*> newFileList = getDirFileList("/home/robert/TESTFOLDER/");
 
         //Then, we check wether the newList is larger than the old one. If so, new files have been added!
-        if(oldFileList.size() < newFileList.size());
+        if(newFileList.size() > oldFileList.size())
         {
             //We create a const char that contains the last element in the newFileList(the file that has been added, and convert it to a string.
             //Note: We do not take any measures to ensure that only .csv files will be handled, since the web interface will provide this constraint.
-            const char* FileChar = newFileList.back();
-            string str(FileChar);
-            printf(FileChar);
+            const char *FileChar = newFileList.back();
+            string FileString = string(FileChar);
+            //Finally, we add our servers' home directory (where we will be storing our .csv files) to the FileString so we have a complete path.
+            string FinalFileString = "/home/robert/TESTFOLDER/" + FileString;
+            //And then we call our piping method.
+
+        }
+        //Some checks for debugging purposes
+        else if(oldFileList.size() > newFileList.size())
+        {
+            printf("FILES HAVE BEEN REMOVED");
+        }
+        else if(oldFileList.size() == newFileList.size())
+        {
+            printf("NO ACTIVITY");
         }
     }
     return 0;
