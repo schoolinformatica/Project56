513830ef6a2376bf782660ce1e9c1b0c13e4e881
Ceesie12345 <ceesjannolen@live.nl>
2015-10-28 15:56:36 +0100

    testing

--- dirListener/main.cpp
+++ dirListener/main.cpp
@@ -31,7 +31,7 @@ vector<const char *> getDirFileList(const char *directory) {
 }
 
 int main() {
-    vector<const char *> oldFileList;
+    vector<const char *> oldFileList  = getDirFileList("/home/cooperatio");
     vector<const char *> newFileList;
     newFileList = getDirFileList("/home/cooperatio");
 
@@ -39,7 +39,7 @@ int main() {
     {
         sleep(1);
         //We create a vector that holds the list of files and dirs BEFORE the 2 second loop
-        oldFileList = newFileList;
+        //oldFileList = newFileList;
         sleep(1);
         //We redo this procedure AFTER the 2 seconds have passed
         newFileList = getDirFileList("/home/cooperatio/");
@@ -47,27 +47,27 @@ int main() {
         cout << "----------------------------------------" << endl;
 
         //Then, we check wether the newList is larger than the old one. If so, new files have been added!
-        if (newFileList.size() > oldFileList.size()) {
-            for (int i = 0 ; i < newFileList.size(); i ++){
-               cout << newFileList.at(i) <<endl;
-
-            }
-
-            cout << "now the old is coming"  << endl;
-
-            for (int i = 0 ; i <  oldFileList.size() ; i ++){
-                cout << oldFileList.at(i) <<  endl;
-            }
-            //We create a const char that contains the last element in the newFileList(the file that has been added, and convert it to a string.
-            //Note: We do not take any measures to ensure that only .csv files will be handled, since the web interface will provide this constraint.
-            const char *FileChar = newFileList.back();
-            string FileString(FileChar);
-            //Finally, we add our servers' home directory (where we will be storing our .csv files) to the FileString so we have a complete path.
-            string FinalFileString = "/home/cooperatio/" + FileString;
-            //And then we call ouCSV-parsing method from our header file
-            cout << "----------------test----------------" << endl;
-            csvreader(FinalFileString);
-        }
+//        if (newFileList.size() > oldFileList.size()) {
+//            for (int i = 0 ; i < newFileList.size(); i ++){
+//               cout << newFileList.at(i) <<endl;
+//
+//            }
+//
+//            cout << "now the old is coming"  << endl;
+//
+//            for (int i = 0 ; i <  oldFileList.size() ; i ++){
+//                cout << oldFileList.at(i) <<  endl;
+//            }
+////            //We create a const char that contains the last element in the newFileList(the file that has been added, and convert it to a string.
+////            //Note: We do not take any measures to ensure that only .csv files will be handled, since the web interface will provide this constraint.
+////            const char *FileChar = newFileList.back();
+////            string FileString(FileChar);
+////            //Finally, we add our servers' home directory (where we will be storing our .csv files) to the FileString so we have a complete path.
+////            string FinalFileString = "/home/cooperatio/" + FileString;
+////            //And then we call ouCSV-parsing method from our header file
+////            cout << "----------------test----------------" << endl;
+////            csvreader(FinalFileString);
+//        }
         break;
     }
     for (int i = 0 ; i < newFileList.size(); i ++){
