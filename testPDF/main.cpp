2dca101c3c4a37831a83598462a1c6229974e66f
Ceesie12345 <ceesjannolen@live.nl>
2015-11-03 20:44:34 +0100

    testing pdf with csv files

---
+++ testPDF/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "listener.h"
+#include "filedatabase.h"
+
+using namespace std;
+
+int main() {
+    //listener("/home/cooperatio/");
+    insert();
+    return 0;
+}
+
