4dadaeac19a75d1375c41b053ab56263b7dbb343
RobinBakker <robinb@live.nl>
2015-11-05 16:45:44 +0100

    improved homepage design

--- http/laravel/resources/views/welcome.blade.php
+++ http/laravel/resources/views/welcome.blade.php
@@ -81,18 +81,21 @@
             </div>
 
         </nav>
+
+
         <!-- Page Content -->
             <div class="container">
 
                 <!-- Heading Row -->
                 <div class="row">
                     <div class="col-md-8">
-                        <img class="img-responsive img-rounded" src="handshake.jpg" alt="">
+                        <img class="img-responsive img-rounded" src="main_pic.jpg" alt="">
                     </div>
                     <!-- /.col-md-8 -->
                     <div class="col-md-4">
-                        <h1>Cooperatio</h1>
+                        <h1>Team Cooperatio</h1>
                         <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+                        <a class="btn btn-primary btn-lg" href="#">Call to Action!</a>
                     </div>
                     <!-- /.col-md-4 -->
                 </div>
@@ -114,21 +117,21 @@
                 <!-- Content Row -->
                 <div class="row">
                     <div class="col-md-4">
-                        <h2>About us</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-                        <a class="btn btn-default" href="/about/">More Info</a>
-                    </div>
-                    <!-- /.col-md-4 -->
-                    <div class="col-md-4">
-                        <h2>Download files</h2>
+                        <h2>Heading 1</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
                         <a class="btn btn-default" href="#">More Info</a>
                     </div>
                     <!-- /.col-md-4 -->
                     <div class="col-md-4">
-                        <h2>Upload files</h2>
+                        <h2>Heading 2</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-                        <a class="btn btn-default" href="/upload/">More Info</a>
+                        <a class="btn btn-default" href="#">More Info</a>
+                    </div>
+                    <!-- /.col-md-4 -->
+                    <div class="col-md-4">
+                        <h2>Heading 3</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+                        <a class="btn btn-default" href="#">More Info</a>
                     </div>
                     <!-- /.col-md-4 -->
                 </div>
@@ -138,12 +141,14 @@
                 <footer>
                     <div class="row">
                         <div class="col-lg-12">
-                            <p>Copyright &copy; Cooperatio 2015</p>
+                            <p>Copyright &copy; Your Website 2014</p>
                         </div>
                     </div>
                 </footer>
 
             </div>
             <!-- /.container -->
+
+
     </body>
 </html>
