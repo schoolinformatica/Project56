edc2605df9dc752a407063acbfb886392f4ce151
Robert <0890289@hr.nl>
2015-11-11 14:06:29 +0100

    php

--- testPDF/mailer.php
+++ testPDF/mailer.php
@@ -3,5 +3,20 @@ $dir = $_POST["dir"];
 
 $msg = "Uw rapport kunt u hier {$dir} downloaden!";
 
-mail("0890289@hr.nl", "Uw rapport-download", $msg);
+if(sendmail())
+{
+	echo "mail sent!";
+}
+else
+{
+	echo "mail not sent";
+}
+
+function sendmail()
+{
+	mail("0890289@hr.nl", "Uw rapport-download", $msg);
+	return true;
+}
+
+
 ?>
