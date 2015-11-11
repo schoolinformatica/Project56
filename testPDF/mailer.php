4c3bc16140280809109d3bdfa3924725881c2d9a
Robert <0890289@hr.nl>
2015-11-11 14:50:24 +0100

    php

--- testPDF/mailer.php
+++ testPDF/mailer.php
@@ -3,9 +3,7 @@ $dir = $_POST["dir"];
 
 $msg = "Uw rapport kunt u hier {$dir} downloaden!";
 
-echo phpinfo();
-
-//mail("0890289@hr.nl", "Uw rapport-download", $msg);
+mail("0890289@hr.nl", "Uw rapport-download", $msg);
 
 
 
