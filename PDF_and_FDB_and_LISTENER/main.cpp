25aca3494982a497204560abfe5454d32ab12309
Robert <0890289@hr.nl>
2015-11-24 11:26:57 +0100

    thrift

--- PDF_and_FDB_and_LISTENER/main.cpp
+++ PDF_and_FDB_and_LISTENER/main.cpp
@@ -3,14 +3,30 @@
 #include "filedatabase.h"
 
 using namespace std;
+bool exists(const string& filename)
+{
+    struct stat statbuffer;
+    return( stat(filename.c_str(), &statbuffer) == 0);
+}
 
-int main() {
-<<<<<<< HEAD:testPDF/main.cpp
-    listener("/home/Cooperatio/");
-=======
-    listener("/home/cooperatio/");
->>>>>>> 85114ae36c40ffdec8432cca4a06a2fa7d018f1d:PDF_and_FDB_and_LISTENER/main.cpp
-    //insert();
+int main(string email, string csvfile) {
+//Check wether file exists
+    if(exists(csvfile) == true)
+    {
+        if(csvfile.find(".csv") != string::npos)
+        {
+            //We are dealing with an FDB file.
+            //We call the CSVReader which converts the file to FDB.
+            //And at the same time we create a PDF from the return value of CSVReader. Neat, eh?
+            //TODO: MAKE SURE THIS PATH IS RIGHT FOR THE SERVER
+	    string path = "/var/www/laravel/storage/uploads/" + csvfile;
+            pdfcreator(csvreader(path));
+        }
+        else
+        {
+            //Not a valid file.
+        }
+    }
     return 0;
 }
 
