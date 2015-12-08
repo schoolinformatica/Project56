c149a3915094fa3584a82986a8917e0edbfbb554
Ceesie12345 <ceesjannolen@live.nl>
2015-12-09 00:28:16 +0100

    added events and monitoring classes

--- input/csv_converter/dbentities/EventEntity.h
+++ input/csv_converter/dbentities/EventEntity.h
@@ -7,8 +7,71 @@
 
 
 class EventEntity {
+private:
+    string date_time;
+    string unit_id;
+    string port;
+    int value;
 
+public:
+    // Database management functions
+    void insert_into_database();
+
+    //Setters
+    void set_date_time(string);
+
+    void set_unit_id(string);
+
+    void set_port(string);
+
+    void set_value(int);
+
+    //Getters
+    string get_date_time();
+
+    string get_unit_id();
+
+    string get_port();
+
+    string get_value();
 };
+
+// Database management functions
+void EventEntity::insert_in_database() {
+    stringstream insert_query;
+
+    insert_query << "INSERT INTO events ";
+    insert_query << "VALUES ('";
+    insert_query << date_time << "','";
+    insert_query << unit_id << "','";
+    insert_query << port << "','";
+    insert_query << value;
+    insert_query << "');";
+
+    Pgsqlcon p;
+    p.exec_none_transaction(insert_query.str());
+}
+
+
+//Setters
+void EventEntity::set_date_time(string Date_time) { date_time = Date_time ;  }
+
+void EventEntity::set_port(string Port) { port= Port; }
+
+void EventEntity::set_unit_id(string Unit_id) { unit_id = Unit_id; }
+
+void EventEntity::set_value(int Value) { value = Value; }
+
+//Getters
+string EventEntity::get_date_time() { return date_time; }
+
+string EventEntity::get_port() { return port; }
+
+string EventEntity::get_unit_id() { return unit_id; }
+
+int EventEntity::get_value() { return value; }
+
+
 
 
 #endif //INPUT_PROCESS_EVENTENTITY_H
--- input/csv_converter/dbentities/MonitoringEntity.h
+++ input/csv_converter/dbentities/MonitoringEntity.h
@@ -6,9 +6,106 @@
 #define INPUT_PROCESS_MONITORINGENTITY_H
 
 
+#include "../pqcon/pgsqlcon.h"
+
 class MonitoringEntity {
+private:
+    string unit_id;
+    string begin_time;
+    string end_time;
+    string type;
+    float min;
+    float max;
+    float sum;
+
+public:
+
+    //Database management function
+    void insert_in_databse();
+
+
+    // Setters
+    void set_unit_id(string);
+
+    void set_begin_time(string);
+
+    void set_end_time(string);
+
+    void set_type(string);
+
+    void set_min(float);
+
+    void set_max(float);
+
+    void set_sum(float);
+
+    // Getters
+    string get_unit_id();
+
+    string get_begin_time();
+
+    string get_end_time();
+
+    string get_type();
+
+    float get_min();
+
+    float get_max();
+
+    float get_sum();
 
 };
+
+void MonitoringEntity::insert_in_databse() {
+    string insert_query;
+
+    insert_query << "INSERT INTO monitoring ";
+    insert_query << "VALUES ('";
+
+    insert_query << unit_id << "', '";
+    insert_query << begin_time << "', '";
+    insert_query << end_time << "', '";
+    insert_query << type << "', '";
+    insert_query << min << "', '";
+    insert_query << max << "', '";
+    insert_query << sum << "'";
+    insert_query << ");";
+
+    Pgsqlcon p;
+    p.exec_none_transaction(insert_query.str());
+
+}
+// Setters
+void MonitoringEntity::set_unit_id(string Unit_id){ unit_id = Unit_id;};
+
+void MonitoringEntity::set_begin_time(string Begin_time){ begin_time = Begin_time;};
+
+void MonitoringEntity::set_end_time(string End_time) { end_time = End_time; };
+
+void MonitoringEntity::set_type(string Type){ type = Type;};
+
+void MonitoringEntity::set_min(float Min){min = Min;};
+
+void MonitoringEntity::set_max(float Max){max = Max;};
+
+void MonitoringEntity::set_sum(float Sum){sum = Sum;};
+
+
+
+// Getters
+string MonitoringEntity::get_unit_id(){ return unit_id; } ;
+
+string MonitoringEntity::get_begin_time(){ return begin_time; };
+
+string MonitoringEntity::get_end_time(){ return end_time;};
+
+string MonitoringEntity::get_type(){ return type;};
+
+float MonitoringEntity::get_min(){ return min;};
+
+float MonitoringEntity::get_max(){ return max;};
+
+float MonitoringEntity::get_sum(){return sum;};
 
 
 #endif //INPUT_PROCESS_MONITORINGENTITY_H
