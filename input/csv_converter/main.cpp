54f217829438965d26563d9f111a96c1e5bb32a9
Steven Schenk <stevenschenk74@hotmail.com>
2015-12-16 14:27:57 +0100

    laravel

--- input/csv_converter/main.cpp
+++ input/csv_converter/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-#include "listener.h"
+//#include "listener.h"
+//#include "dbpusher/dbpusher.h"
+#include "pqcon/pgsqlcon.h"
 
 using namespace std;
 
 int main() {
+    Pgsqlcon p;
+    p.exec_none_transaction("SELECT * FROM connections");
+//    push_list_to_database("connections.csv");
     //listener("/home/cooperatio/");
     return 0;
 }
