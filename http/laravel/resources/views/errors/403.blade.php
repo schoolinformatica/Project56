0025cf57c9e18696e6f597a2d47e43fa0d6b4848
RobinBakker <robinb@live.nl>
2016-01-06 12:55:41 +0100

    Added 403 acces denied errorpage

---
+++ http/laravel/resources/views/errors/403.blade.php
@@ -0,0 +1,14 @@
+@extends("template")
+@section("title")
+    403
+    @stop
+@section("content")
+
+                <!-- Row -->
+                <div class="row">
+                    <div class="title">Access denied!</div>
+                </div>
+                <!-- /.row -->
+
+
+@stop
