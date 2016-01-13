fc532c8598a7de51610abadcd703ac0cd2626ba8
Steven Schenk <stevenschenk74@hotmail.com>
2016-01-13 13:43:34 +0100

    link fix

--- http/laravel/public/PDFMaker/processing/downloadFormHandler.php
+++ http/laravel/public/PDFMaker/processing/downloadFormHandler.php
@@ -20,7 +20,7 @@ if(isset($_GET['email'])){
 				foreach($out as $line){
 					echo $line . "\n" ;
 				}
-				header("Location: http://145.24.222.182/download?succes=1");
+				redirect_now("/download?succes=1");
 			}
 			//catch the exceptions if there are some.. 
 			catch(Exception $tx) {				
@@ -39,3 +39,29 @@ else {
 	echo 'email is not given!';
 }
 
+
+/**
+ * Redirect the user no matter what. No need to use a return
+ * statement. Also avoids the trap put in place by the Blade Compiler.
+ *
+ * @param string $url
+ * @param int $code http code for the redirect (should be 302 or 301)
+ */
+function redirect_now($url, $code = 302)
+{
+	try {
+		\App::abort($code, '', ['Location' => $url]);
+	} catch (\Exception $exception) {
+		// the blade compiler catches exceptions and rethrows them
+		// as ErrorExceptions :(
+		//
+		// also the __toString() magic method cannot throw exceptions
+		// in that case also we need to manually call the exception
+		// handler
+		$previousErrorHandler = set_exception_handler(function () {
+		});
+		restore_error_handler();
+		call_user_func($previousErrorHandler, $exception);
+		die;
+	}
+}
