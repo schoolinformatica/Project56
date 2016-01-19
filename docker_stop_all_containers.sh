c753dbfcd87c9538bb6b8820257fac6a1b9dfb82
Steven Schenk <stevenschenk74@hotmail.com>
2016-01-19 01:18:19 +0100

    modified docker_stop_all_containers to just stop specifig containers

--- docker_stop_all_containers.sh
+++ docker_stop_all_containers.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+RUNNING=$(docker inspect --format="{{ .State.Running }}" postgres 2> /dev/null)
+
+if [ "$RUNNING" == "true" ]; then
+  sudo docker stop postgres
+  sudo docker rm postgres
+fi
+
+if [ "$RUNNING" == "false" ]; then
+  sudo docker rm postgres
+fi
+
+RUNNING=$(docker inspect --format="{{ .State.Running }}" webserver 2> /dev/null)
+
+if [ "$RUNNING" == "true" ]; then
+  sudo docker stop webserver
+  sudo docker rm webserver
+fi
+
+if [ "$RUNNING" == "false" ]; then
+  sudo docker rm webserver
+fi
