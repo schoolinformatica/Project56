1fad01091447da29dcc8fec5619426c3accc8ace
Cees-Jan Nolen <0890289@hr.nl>
2016-01-19 01:26:02 +0100

    fix

--- scriptipchange.sh
+++ scriptipchange.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
-1=$(docker inspect $CID | grep IPAddress | cut -d '"' -f 4)
+ip=$(docker inspect $CID | grep IPAddress | cut -d '"' -f 4)
 
 replacestring=$(cat .env | grep DB_HOST )
-newstring='DB_HOST='$1
+newstring='DB_HOST='$ip
 
 sed -i 's/'$replacestring'/'$newstring'/g' .env
 
 
 replacestring1=$(cat credentials.pgconf | grep hostip )
-newstring1='hostip: '$1
+newstring1='hostip: '$ip
 
 sed -i "s/$replacestring1/$newstring1/g" credentials.pgconf
 
