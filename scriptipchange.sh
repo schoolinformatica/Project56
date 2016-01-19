39de4935cbe390aa26361973011801dc28656282
Cees-Jan Nolen <0890289@hr.nl>
2016-01-19 01:12:23 +0100

    ip change

--- scriptipchange.sh
+++ scriptipchange.sh
@@ -1,13 +1,11 @@
 #!/bin/bash 
 
-cd ~/http/laravel
 
 replacestring=$(cat .env | grep DB_HOST )
 newstring='DB_HOST='$1
 
 sed -i 's/'$replacestring'/'$newstring'/g' .env
 
-cd ~/processing
 
 replacestring1=$(cat credentials.pgconf | grep hostip )
 newstring1='hostip: '$1
